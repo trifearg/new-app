0f0dd929bc288923afa68b1f715a17d733bffcc6
triferg <npzbs9tont@mail.ru>
2021-07-23 13:21:18 +0400

    Добавил новые файлы

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-import ns from '../Pictures/ns.jpg'
 import gazeta from '../Pictures/gazeta.jpg'
 
 export const Home = () => (
@@ -8,7 +7,7 @@ export const Home = () => (
         <Container style={{ marginRight: '0', marginTop: '15px' }}>
             <Row>
                 <Col md={5}>
-                    <img src={gazeta} width={450} />
+                    <img src={gazeta} alt="gazeta" width={450} />
                 </Col>
                 <Col md={6}>
                     <h2>Добро пожаловать!</h2>
--- src/Components/News.js
+++ src/Components/News.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Card, Container, Spinner } from 'react-bootstrap';
+import { Card, Container, Spinner, Row } from 'react-bootstrap';
 import styled from 'styled-components';
 import { useState } from 'react';
 const Styles = styled.div`
@@ -26,12 +26,16 @@ export function News() {
     const [items, setItems] = useState([]);
 
     useEffect(() => {
-        fetch("https://newsapi.org/v2/everything?language=ru&q=tesla&sortBy=popularity&apiKey=a0d1bff76be44cb5b0f9716537d62bc3")
+        fetch("https://newsapi.org/v2/everything?language=ru&q=tesla&apiKey=a0d1bff76be44cb5b0f9716537d62bc3")
             .then(res => res.json())
             .then(
                 (result) => {
                     setIsLoaded(true);
-                    setItems(result.articles);
+                    setItems(result.articles.map(({ source, author, content, ...propertys }) => propertys)
+                        .splice(0, 8)
+                        .sort(function (a, b) {
+                            return new Date(b.publishedAt) - new Date(a.publishedAt);
+                        }));
                 },
                 (error) => {
                     setIsLoaded(true);
@@ -39,6 +43,8 @@ export function News() {
                 }
             )
     }, [])
+
+    console.log(items);
 
     if (error) {
         return <div>Error: {error.message}</div>;
@@ -49,18 +55,20 @@ export function News() {
             <>
                 <Styles>
                     <Container>
-                        {items.map((item) => (
-                            <Card border="dark" style={{ margin: "10px auto", width: "800px" }}>
-                                <Card.Img variant="top" src={item.urlToImage} />
-                                <Card.Body>
-                                    <Card.Title>{item.title}</Card.Title>
-                                    <Card.Text>{item.description}</Card.Text>
-                                </Card.Body>
-                                <Card.Footer>
-                                    <small className="text-muted"><a href={item.url} target="_blank"> {item.url}</a></small>
-                                </Card.Footer>
-                            </Card>
-                        ))}
+                        <Row xs="4">
+                            {items.map((item, index) => (
+                                <Card key={index}>
+                                    <Card.Img variant="top" src={item.urlToImage} />
+                                    <Card.Body>
+                                        <Card.Title>{item.title}</Card.Title>
+                                        <Card.Text>{item.description}</Card.Text>
+                                    </Card.Body>
+                                    <Card.Footer>
+                                        <a rel="noopener noreferrer" href={item.url} target="_blank">{item.url}</a>
+                                    </Card.Footer>
+                                </Card>
+                            ))}
+                        </Row>
                     </Container>
                 </Styles>
             </>
